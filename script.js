4fdbc9c9c0103bea911be966c5486913ce2d1d6e
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-01-20 20:40:17 +0100

    Styled grid and added random color

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 const container = document.getElementById("container");
 for (let i = 0; i < 16; i++) {
-    const divToAppend = document.createElement("div");
-    divToAppend.className = "square"
+    for (let j = 0; j < 16; j++) {
+        const divToAppend = document.createElement("div");
+        divToAppend.className = "square"
 
-    divToAppend.style.width = "150px";
-    divToAppend.style.height = "150px";
-    divToAppend.style.border = "2px solid crimson";
-    divToAppend.style.borderRadius = "5px"
+        divToAppend.style.width = "14px";
+        divToAppend.style.height = "14px";
+        divToAppend.style.border = "1px solid black";
 
-    container.append(divToAppend);
+        container.append(divToAppend);
+    }
 }
 
 function changeBackgroundColor() {
-    this.style.backgroundColor = "red";
+    let style = window.getComputedStyle(this);
+    let backgroundColor = style.backgroundColor;
+    console.log(backgroundColor);
+    if (backgroundColor === "rgb(0, 0, 0)" || backgroundColor === "rgba(0, 0, 0, 0)")
+        this.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
-function resetBackgroundColor() {
-    this.style.backgroundColor = "white";
-}
 
 const squares = document.getElementsByClassName("square");
 for (const square of squares) {
-    square.addEventListener("click", changeBackgroundColor);
+    square.addEventListener("mouseover", changeBackgroundColor);
 }
