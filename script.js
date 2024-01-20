04736ce63d18261f13bab4ad161dc067a1f4eb66
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-01-20 20:16:35 +0100

    Add eventlisteners for hoovering over

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const container = document.getElementById("container");
 for (let i = 0; i < 16; i++) {
-    const divToApped = document.createElement("div");
+    const divToAppend = document.createElement("div");
+    divToAppend.className = "square"
 
-    divToApped.style.width = "150px";
-    divToApped.style.height = "150px";
-    divToApped.style.border = "2px solid crimson";
-    divToApped.style.borderRadius = "5px"
+    divToAppend.style.width = "150px";
+    divToAppend.style.height = "150px";
+    divToAppend.style.border = "2px solid crimson";
+    divToAppend.style.borderRadius = "5px"
 
+    container.append(divToAppend);
+}
 
-    container.append(divToApped);
+function changeBackgroundColor() {
+    this.style.backgroundColor = "red";
+}
+
+function resetBackgroundColor() {
+    this.style.backgroundColor = "white";
+}
+
+const squares = document.getElementsByClassName("square");
+for (const square of squares) {
+    square.addEventListener("click", changeBackgroundColor);
 }
